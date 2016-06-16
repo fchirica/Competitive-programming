36d99595155a0d3bf2860b142ee26775cca28cae
Florin Chirica <florinc_best@yahoo.com>
2016-06-16 23:10:17 +0300

    Deleted some irrelevant folders

--- Samples/Helloworld/Helloworld.cpp
+++
@@ -1,9 +0,0 @@
-// Helloworld : Console application project sample.
-
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
-}
--- Samples/MessageBox/MessageBox.cpp
+++
@@ -1,9 +0,0 @@
-// MessageBox : Windows application project sample
-
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	MessageBox(NULL, "Hello, Windows!", "Hello!", MB_OK);
-	return 0;
-}
